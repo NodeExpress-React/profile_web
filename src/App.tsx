87ea183760dd6227c85a42007c01346f058bf8ea
joaoGlima09 <joao.glima@sptech.school>
2024-10-08 11:51:50 -0300

    build: :pushpin: initial construction project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Cadastro from "./pages/Cadastro";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Cadastro />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Cadastro/index.tsx
@@ -0,0 +1,20 @@
+function Cadastro() {
+  return (
+    <>
+      <h2>Cadastro</h2>
+      <form action="">
+        <label htmlFor="">
+          <input type="text" />
+        </label>
+        <label htmlFor="">
+          <input type="email" />
+        </label>
+        <label htmlFor="">
+          <input type="password" />
+        </label>
+      </form>
+    </>
+  );
+}
+
+export default Cadastro;
