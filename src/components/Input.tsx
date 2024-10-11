4a967010969cbb50e3ccf2205af9f889d79ac8f6
joaoGlima09 <joao.glima@sptech.school>
2024-10-11 16:07:40 -0300

    style: :art: placeholder mask

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface InputProps {
   type: string;
@@ -7,15 +7,30 @@ interface InputProps {
   name: string;
 }
 
-const Input: React.FC<InputProps> = ({ type, placeholder, register, name }) => (
-  <label>
-    <input
-      {...register(name, { required: true })}
-      type={type}
-      placeholder={placeholder}
-      className="w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none"
-    />
-  </label>
-);
+const Input: React.FC<InputProps> = ({ type, placeholder, register, name }) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [hasValue, setHasValue] = useState(false);
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    setIsFocused(false);
+    setHasValue(e.target.value !== "");
+  };
+
+  return (
+    <label className="relative w-full">
+      <input
+        {...register(name, { required: true })}
+        type={type}
+        className="w-full p-3 rounded-lg border border-gray-300 focus:outline-none"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
+      <span className={`absolute left-3 inset-y-1/4 duration-200 text-gray-500 ${isFocused || hasValue ? "left-0 top-0 text-xs" : ""}`}>
+        {placeholder}
+      </span>
+    </label>
+  );
+};
 
 export default Input;
