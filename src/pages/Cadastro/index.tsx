f074c738449f90cd15bb28fcea1499ec945cc47e
joaoGlima09 <joao.glima@sptech.school>
2024-10-11 16:08:04 -0300

    feat: :zap: add requests for axios

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,51 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import Input from "../../components/Input.tsx";
 import Button from "../../components/Button.tsx";
+import api from "../../services/api.ts";
 
 function Cadastro() {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data: any) => console.log(data);
+  const navigate = useNavigate();
+
+  const onSubmit = async (data: any) => {
+    try {
+      await api.post("/cadastro", {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      });
+      alert("Usuário cadastrado com sucesso");
+
+      navigate("/login");
+    } catch (err) {
+      alert("Erro ao cadastrar");
+    }
+  };
 
   return (
     <>
       <section className="w-screen h-screen bg-bg-cad bg-cover bg-center">
         <div className="w-6/12 h-full bg-white shadow-xl flex flex-col gap-20 items-center justify-center">
           <h2 className="text-5xl font-bold">Cadastre seu perfil</h2>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="w-4/12 flex flex-col gap-4"
-          >
-            <label>
-              <Input
-                register={register}
-                name="name"
-                type="text"
-                placeholder="Nome"
-              />
-            </label>
-            <label>
-              <Input
-                register={register}
-                name="email"
-                type="email"
-                placeholder="E-mail"
-              />
-            </label>
-            <label>
-              <Input
-                register={register}
-                name="password"
-                type="password"
-                placeholder="Senha"
-              />
-            </label>
+          <form onSubmit={handleSubmit(onSubmit)} className="w-4/12 flex flex-col gap-4">
+            <Input register={register} name="name" type="text" placeholder="Nome" />
+            <Input register={register} name="email" type="email" placeholder="E-mail" />
+            <Input register={register} name="password" type="password" placeholder="Senha" />
             <Button text="Cadastre-se" />
           </form>
-          <Link
-            to="/login"
-            className="font-medium text-sky-400 hover:text-sky-600 ease-in duration-150 focus:outline-none focus:text-sky-600"
-          >
+          <Link to="/login" className="font-medium text-sky-400 hover:text-sky-600 ease-in duration-150 focus:outline-none focus:text-sky-600">
             Já tem uma conta? Faça login
           </Link>
         </div>
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/Input.tsx";
 import Button from "../../components/Button.tsx";
 import { useForm } from "react-hook-form";
+import api from "../../services/api.ts";
 
 function Login() {
   const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
 
-  const onSubmit = (data: any) => {
-    console.log(data);
+  const onSubmit = async (data: any) => {
+    try {
+      const { data: token } = await api.post("/login", {
+        email: data.email,
+        password: data.password,
+      });
+      localStorage.setItem("token", token);
+
+      navigate("/perfil");
+    } catch (err) {
+      alert("Senha ou Email incorretos!");
+    }
   };
 
   return (
@@ -15,33 +27,12 @@ function Login() {
       <section className="w-screen h-screen bg-bg-log bg-cover bg-center flex flex-row-reverse">
         <div className="w-6/12 h-full bg-white shadow-xl flex flex-col gap-20 items-center justify-center">
           <h2 className="text-5xl font-bold">Faça seu Login</h2>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="w-4/12 flex flex-col gap-4"
-          >
-            <label>
-              <Input
-                register={register}
-                name="email"
-                type="email"
-                placeholder="E-mail"
-              />
-            </label>
-            <label>
-              <Input
-                register={register}
-                name="password"
-                type="password"
-                placeholder="Senha"
-              />
-            </label>
-
+          <form onSubmit={handleSubmit(onSubmit)} className="w-4/12 flex flex-col gap-4">
+            <Input register={register} name="email" type="email" placeholder="E-mail" />
+            <Input register={register} name="password" type="password" placeholder="Senha" />
             <Button text="Entrar" />
           </form>
-          <Link
-            to="/"
-            className="font-medium text-sky-400 hover:text-sky-600 ease-in duration-150 focus:outline-none focus:text-sky-600"
-          >
+          <Link to="/" className="font-medium text-sky-400 hover:text-sky-600 ease-in duration-150 focus:outline-none focus:text-sky-600">
             Não possuí uma conta? Faça seu cadastro
           </Link>
         </div>
