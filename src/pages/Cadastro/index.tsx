310cba4e7572ce32f44440afaa4d0224b795e6b6
joaoGlima09 <joao.glima@sptech.school>
2024-10-11 13:46:01 -0300

    feat: :zap: form in cad

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,17 +1,45 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import Input from "../../components/Input.tsx";
 import Button from "../../components/Button.tsx";
 
 function Cadastro() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <>
       <section className="w-screen h-screen bg-bg-cad bg-cover bg-center">
-        <div className="w-6/12 h-full bg-white opacity-90 shadow-xl flex flex-col gap-20 items-center justify-center">
+        <div className="w-6/12 h-full bg-white shadow-xl flex flex-col gap-20 items-center justify-center">
           <h2 className="text-5xl font-bold">Cadastre seu perfil</h2>
-          <form className="w-4/12 flex flex-col gap-4">
-            <Input id="input_name" type="text" placeholder="Nome" />
-            <Input id="input_email" type="email" placeholder="E-mail" />
-            <Input id="input_password" type="password" placeholder="Senha" />
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="w-4/12 flex flex-col gap-4"
+          >
+            <label>
+              <Input
+                register={register}
+                name="name"
+                type="text"
+                placeholder="Nome"
+              />
+            </label>
+            <label>
+              <Input
+                register={register}
+                name="email"
+                type="email"
+                placeholder="E-mail"
+              />
+            </label>
+            <label>
+              <Input
+                register={register}
+                name="password"
+                type="password"
+                placeholder="Senha"
+              />
+            </label>
             <Button text="Cadastre-se" />
           </form>
           <Link
