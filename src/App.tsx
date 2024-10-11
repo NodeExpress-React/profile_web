64b55584d87af0870bac854cb05668013e0c913c
joaoGlima09 <joao.glima@sptech.school>
2024-10-11 13:46:55 -0300

    build: :memo: new route - profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
 import Login from "./pages/Login";
+import Perfil from "./pages/Perfil";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Cadastro />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/perfil" element={<Perfil />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Perfil/index.tsx
@@ -0,0 +1,11 @@
+function Perfil() {
+  return (
+    <>
+      <section className="w-screen h-screen bg-bg-log bg-cover bg-center flex flex-row-reverse">
+        <div className="w-6/12 h-full bg-white shadow-xl flex flex-col gap-20 items-center justify-center"></div>
+      </section>
+    </>
+  );
+}
+
+export default Perfil;
